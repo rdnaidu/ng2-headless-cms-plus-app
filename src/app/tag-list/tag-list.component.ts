29eb9eb982d3ccb6ee1310d8c922402a523d49c3
tsukhu <tsukhu@hcl.com>
2016-05-17 12:06:22 +0530

    tag list initial version with search

--- src/app/tag-list/tag-list.component.ts
+++ src/app/tag-list/tag-list.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {MdButton} from '@angular2-material/button';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 import {RouteConfig, ROUTER_DIRECTIVES, Router} from '@angular/router-deprecated';
+import {TYPEAHEAD_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 import {TagListService} from './tag-list.service';
 import {Tag} from './tag';
@@ -9,14 +10,20 @@ import {Tag} from './tag';
 @Component({
   selector: 'tag-list',
   template: require('./tag-list.component.html'),
-  directives: [ROUTER_DIRECTIVES, MD_LIST_DIRECTIVES, MdButton],
+  directives: [
+    ROUTER_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    TYPEAHEAD_DIRECTIVES,
+    MdButton],
   providers: [TagListService]
 })
 export class TagListComponent {
   tags: any[];
+  allTags: any[];
   tagsLoading;
   tagsServiceError = false;
   errorMessage;
+  selected: string = '';
 
   constructor(private _service: TagListService) { }
 
@@ -24,14 +31,18 @@ export class TagListComponent {
     this.loadTags();
   }
 
+  public typeaheadOnSelect($e: Event): void {
+    console.log($e.item.tag);
+  }
+
   private loadTags(): void {
     this.tagsLoading = true;
     this._service.getTags()
       .subscribe(
       tags => {
-     //   this.tags = tags;
+        this.allTags = tags;
         this.tags = _.take(_.sortBy(tags, 'count').reverse(), 20);
-     //   console.log(this.tags);
+        //   console.log(this.tags);
       },
       error => {
         this.tagsServiceError = true;
@@ -42,5 +53,7 @@ export class TagListComponent {
         this.tagsLoading = false;
       });
   }
+
+
 
 }
