5f510649163f630a9b4dbf064befd22857481d5e
tsukhu <tsukhu@hcl.com>
2016-06-03 17:31:24 +0530

    minor update

--- src/app/omdb/omdb.ts
+++ src/app/omdb/omdb.ts
@@ -2,7 +2,7 @@ export class OmdbSearchParams {
     s: string;          // Movie title to search for.
     type: string;      // Type of result to return. movie, series, episode
     page: number;      // Page number to return. 1-100 
-    
+
 }
 
 export enum MediaTypes {
