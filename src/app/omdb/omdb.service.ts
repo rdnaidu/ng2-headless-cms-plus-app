c1c96b751a6c7e72d4f071cc1b00bb5453aa3c2a
tsukhu <tsukhu@hcl.com>
2016-06-04 17:28:00 +0530

    Omdb API2 service creation

--- src/app/omdb/omdb.service.ts
+++ src/app/omdb/omdb.service.ts
@@ -2,12 +2,12 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import { OmdbSearchParams } from './omdb';
+import { OmdbSearchParams, OmdbIDSearchParams } from './omdb';
 
 @Injectable()
 export class OmdbService {
 	private _url = 'http://www.omdbapi.com/';
-	
+
 
 	constructor(private _http: Http) {
 
@@ -16,18 +16,37 @@ export class OmdbService {
 	getDetails(params?: OmdbSearchParams) {
 		let url = this._url;
 
-	//	if (filter && filter.userId)
-	//		url += '?userId=' + filter.userId;
+		//	if (filter && filter.userId)
+		//		url += '?userId=' + filter.userId;
 		if (params && params.s != '') {
-			url += '?s='+ params.s;
+			url += '?s=' + params.s;
 		}
-		
+
 		if (params && params.page != undefined) {
-			url += '&page='+ params.page;
+			url += '&page=' + params.page;
 		}
 		//console.log(url);
 		return this._http.get(url)
-				.map(res => res.json());
+			.map(res => res.json());
+	}
+
+	getDetailsById(params?: OmdbIDSearchParams) {
+		let url = this._url;
+		
+		if (params && params.i != '') {
+			url += '?i=' + params.i;
+		}
+
+		if (params && params.plot != undefined) {
+			url += '&plot=' + params.plot;
+		}
+
+		if (params && params.tomatoes != undefined) {
+			url += '&tomatoes=' + params.tomatoes;
+		}
+		//console.log(url);
+		return this._http.get(url)
+			.map(res => res.json());
 	}
 
 }
