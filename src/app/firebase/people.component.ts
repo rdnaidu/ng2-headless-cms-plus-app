52061439c5c9c0d02caf14fe7d98beb6adeb6a0d
tsukhu <tsukhu@hcl.com>
2016-07-06 10:51:13 +0530

    Added Delete functionality

--- src/app/firebase/people.component.ts
+++ src/app/firebase/people.component.ts
@@ -1,17 +1,21 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { AngularFire } from 'angularfire2';
 import { Observable } from 'rxjs';
+import { SpinnerComponent } from '../shared/spinner.component';
 import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'people',
   styleUrls: ['./people.component.scss'],
   templateUrl: './people.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  directives: [SpinnerComponent]
 })
 export class PeopleComponent {
   people: Observable<any[]>;
+  isLoading: boolean;
   constructor(public af: AngularFire) {
+    this.isLoading = true;
     this.people = af.database.list('/people')
     .map((people) => {
       return people.map((person) => {
@@ -24,5 +28,38 @@ export class PeopleComponent {
 
   ngOnInit() {
     console.log('hello `People` component');
+  }
+
+  remove(person) {
+    //alert(`${person.$key}`);
+
+    const itemObservable = this.af.database.object(`/todos/${person.$key}`);
+    itemObservable.remove()
+      .then( _ => {
+      //  console.log('success');
+        this.removePerson(person);
+      })
+      .catch(err => console.log(err, 'Error in deletion'));
+
+  }
+
+  removePerson(person) {
+    const itemObservable = this.af.database.object(`/people/${person.$key}`);
+     itemObservable.remove()
+      .then( _ => {
+      //  console.log('success');
+        this.removePerson(person);
+      })
+      .catch(err => console.log(err, 'Error in deletion'));
+  }
+
+  removeTodo(person, todo) {
+  //    alert(`${todo.$key}`);
+   const itemObservable = this.af.database.object(`/todos/${person.$key}/${todo.$key}`);
+    itemObservable.remove()
+      .then( _ => {
+     //   console.log('success');
+      })
+      .catch(err => console.log(err, 'Error in deletion'));
   }
 }
