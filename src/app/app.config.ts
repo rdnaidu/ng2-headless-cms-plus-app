ae2153896ae789b865e90b003cfd18b642f84367
Tarun Sukhu <tsukhu71@gmail.com>
2016-08-02 09:22:29 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,12 +22,12 @@ export const CONFIG: Config = {
     solrRootURL: 'http://10.146.201.83:8080/solr/collection1',
     stubURL: '/assets/blogs-json',
     omdbURL: 'http://www.omdbapi.com/',
-    fbAppEnabled: true,
+    fbAppEnabled: false,
     fbAppConfig: {
-        apiKey: 'AIzaSyDB80HQLA-MuGym6r1dIcQcdI67eE0DI3Q',
-        authDomain: 'xperience-f9109.firebaseapp.com',
-        databaseURL: 'https://xperience-f9109.firebaseio.com',
-        storageBucket: 'xperience-f9109.appspot.com',
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        storageBucket: '',
     }
 };
 
